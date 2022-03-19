910ebf144ba8d5e75abee63421cacb861840a66e
zlxiao97 <zlxiao97@yahoo.com>
2022-03-19 17:19:37 +0800

    feat: connect button

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,4 @@
-export const homePath = "/connect";
+export const homePath = "/preSale";
 
 export default [
   // Dashboard 仅为 demo
@@ -7,12 +7,12 @@ export default [
   //   path: "dashboard",
   //   component: require("../pages/Dashboard").default
   // },
-  {
-    name: "connect",
-    path: "connect",
-    component: require("../vendors/pages/IDO").default,
-    hideInMenu: true
-  },
+  // {
+  //   name: "connect",
+  //   path: "connect",
+  //   component: require("../vendors/pages/IDO").default,
+  //   hideInMenu: true
+  // },
   {
     name: "PreSale",
     path: "preSale",

15295334266a6d48af7546318c276812f6a66442
zlxiao97 <zlxiao97@yahoo.com>
2022-04-03 18:37:29 +0800

    feat: add home page

--- src/components/PageLayout/index.js
+++ src/components/PageLayout/index.js
@@ -11,7 +11,7 @@ export default (props) => {
     width: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
     cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
     rowHeight: 100,
-    margin: [20, 20]
+    margin: [0, 0]
   };
   return (
     <ResponsiveGridLayout {...defaultProps} {...props}>
---
+++ src/pages/Home/Stateless/modules/M6/index.js
@@ -0,0 +1,60 @@
+import { Col, Row, Typography } from "antd";
+import m61Image from "@/assets/m61.png";
+import m62Image from "@/assets/m62.png";
+import m63Image from "@/assets/m63.png";
+import m64Image from "@/assets/m64.png";
+import m65Image from "@/assets/m65.png";
+import m66Image from "@/assets/m66.png";
+import styles from "./index.module.less";
+
+const { Title } = Typography;
+
+export default () => {
+  return (
+    <div className={styles.container}>
+      <Row>
+        <Col offset={6} span={12}>
+          <Title level={3}>
+            Join our growing ecosystem of leading infrastructure partners,
+            liquidity platforms, data providers and more!
+          </Title>
+        </Col>
+      </Row>
+
+      <div className={styles.brands}>
+        <Row gutter={[70,70]}>
+          <Col offset={4} span={5}>
+            <div className={styles.brands__item}>
+              <img src={m61Image} />
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className={styles.brands__item}>
+              <img src={m62Image} />
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className={styles.brands__item}>
+              <img src={m63Image} />
+            </div>
+          </Col>
+          <Col offset={4} span={5}>
+            <div className={styles.brands__item}>
+              <img src={m64Image} />
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className={styles.brands__item}>
+              <img src={m65Image} />
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className={styles.brands__item}>
+              <img src={m66Image} />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};

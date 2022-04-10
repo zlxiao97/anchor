5b8dd50eabad4aabf6b91bd9e8b89c47b6b96a04
zlxiao97 <zlxiao97@yahoo.com>
2022-04-10 14:59:23 +0800

    feat: change ico

--- src/hooks/useWeb3Connect.js
+++ src/hooks/useWeb3Connect.js
@@ -6,8 +6,8 @@ import { useWeb3Context } from "@/vendors/hooks/web3-context";
 export default () => {
   const navigate = useNavigate();
   const { address, connect } = useWeb3Context();
-  useEffect(() => {
-    if (address) navigate(PATH_AFTER_CONNECT);
-  }, [address]);
+  // useEffect(() => {
+  //   if (address) navigate(PATH_AFTER_CONNECT);
+  // }, [address]);
   return [address, connect];
 };

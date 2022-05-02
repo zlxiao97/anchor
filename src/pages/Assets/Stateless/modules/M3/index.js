e5e9dd02aece329b5a47a15cee1b8c46470b61af
zlxiao97 <zlxiao97@yahoo.com>
2022-05-02 21:00:58 +0800

    feat: assets layout

--- src/pages/Assets/Stateless/modules/M3/index.js
+++ src/pages/Assets/Stateless/modules/M3/index.js
@@ -1,58 +1,58 @@
-import { Button, Form, Input } from "antd";
-import inputPrefix from "@/assets/inputPrefix.png";
-import inputSuffix from "@/assets/inputSuffix.png";
-import Statistic from "@/components/Statistic";
+import { Button, Form, Input, Typography } from "antd";
 import styles from "./index.module.less";
 
-const IconImage = ({ src, onClick }) => (
-  <img className={styles.icon} src={src} onClick={onClick} />
-);
+const { Title } = Typography;
 
 export default (props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.container__border}>
-        <span>Activation Code</span>
-      </div>
-      <div>
-        <Form form={props.form} layout="inline">
-          <Form.Item name="count">
-            <Input className={styles.container__input} />
-          </Form.Item>
-          <Form.Item>
-            <Button className={styles.container__btn} onClick={props.userMint}>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-        <p className={styles.container__info}>input activation code</p>
-      </div>
-      <div>
-        <Form form={props.form} layout="inline">
-          <Form.Item name="count">
-            <Input className={styles.container__input} />
-          </Form.Item>
-          <Form.Item>
-            <Button className={styles.container__btn} onClick={props.userMint}>
-              Get
-            </Button>
-          </Form.Item>
-        </Form>
-        <p className={styles.container__info}>Create your activation code</p>
-      </div>
-      <div className={styles.container__value}>
-        <Statistic
-          layout="horizontal"
-          title="Number of Invitees"
-          value={0}
-          precision={0}
-        />
-        <Statistic
-          layout="horizontal"
-          title="Earn Expand Factor"
-          value={1.0}
-          precision={1}
-        />
+      <Title level={5}>Activation Code</Title>
+      <div className={styles.container__content}>
+        <div className={styles.form}>
+          <div className={styles.form__group}>
+            <div className={styles.form__field}>
+              <Input
+                className={styles.form__input}
+                value={props.activationCode}
+                onChange={props.onActivationCodeChange}
+              />
+              <Button
+                className={styles.form__btn}
+                onClick={props.onSubmitClick}
+              >
+                Submit
+              </Button>
+            </div>
+            <span className={styles.form__tip}>input activation code</span>
+          </div>
+          <div className={styles.form__group}>
+            <div className={styles.form__field}>
+              <Input
+                className={styles.form__input}
+                value={props.createCode}
+                onChange={props.onCreateCodeChange}
+              />
+              <Button className={styles.form__btn} onClick={props.onGetClick}>
+                Get
+              </Button>
+            </div>
+            <span className={styles.form__tip}>
+              Create your activation code
+            </span>
+          </div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.statistic__item}>
+            <span className={styles.statistic__label}>Number of Invitess:</span>
+            <span className={styles.statistic__value}>0</span>
+          </div>
+          <div className={styles.statistic__item}>
+            <span className={styles.statistic__label}>Earn Expand Factor:</span>
+            <span className={styles.statistic__value}>
+              {Number(1).toFixed(1)}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
